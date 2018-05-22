a97a72fcfbd568475e3e66a00b52c7d40d5c6dd1
Du <tam300497@gmail.com>
2018-05-22 16:23:45 +0700

    Update main-header feature

---
+++ src/app/shared/main-header/assets.ts
@@ -0,0 +1,10 @@
+import {  RouteInfo } from './domain';
+
+export const ROUTES: RouteInfo[] = [
+
+    { title: 'PROGRAMS', routerUrl: '/programs', class: ['Body rental', 'Training', 'Audits', 'Business'] },
+    { title: 'FACILITIES', routerUrl: '/facilities', class: ['CRO', 'ANALYTICS', 'ARC'] },
+    { title: 'GROUP', routerUrl: '/group', class: ['TEAM', 'FRIEND', 'STYLE'] },
+    { title: 'EVENTS', routerUrl: '/events', class: ['APRIL', 'MAY'] },
+    { title: 'MEMBER', routerUrl: '/member', class: ['LOG-IN', 'REGISTER'] },
+];
