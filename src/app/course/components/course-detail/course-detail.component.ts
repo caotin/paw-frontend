e2ef63b31f86c0c5e1de4bd2b5838e30be0ba650
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2018-07-07 15:24:25 +0700

    Fix dropdown

--- src/app/course/components/course-detail/course-detail.component.ts
+++ src/app/course/components/course-detail/course-detail.component.ts
@@ -6,31 +6,32 @@ import { CourseService } from '../../../core/services/course.service';
     selector: 'course-detail',
     templateUrl: './course-detail.component.html',
     styleUrls: ['./course-detail.component.css'],
-    providers:[CourseService],
+    providers: [CourseService],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseDetailComponent implements OnInit {
 
-    params:any;
+    params: any;
     ngOnInit(): void {
         this.params = this.activatedRoute.snapshot.params;
         this.courseService.getCourse(this.params.id).subscribe(
-            data=>{this.course=data
-            // console.log(data);
+            data => {
+                this.course = data;
+                // console.log(data);
             }
         );
         setInterval(() => {
             this.ref.markForCheck();
         }, 1000);
     }
-    course: any ;
+    course: any;
 
     start: string;
 
-    stringSologan1: string="“WE CAN SWIM!”";
-    stringSologan2: string="“Dạy bơi kèm riêng cam kết chất lượng!” – Chúng tôi đã, đang và sẽ theo đúng định hướng “đơn giản” này.";
+    stringSologan1: string = '“WE CAN SWIM!”';
+    stringSologan2: string = '“Dạy bơi kèm riêng cam kết chất lượng!” – Chúng tôi đã, đang và sẽ theo đúng định hướng “đơn giản” này.';
 
-    constructor(private activatedRoute:ActivatedRoute,private courseService:CourseService,private ref: ChangeDetectorRef) {
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService, private ref: ChangeDetectorRef) {
 
     }
 
